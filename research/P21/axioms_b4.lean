db57d6412da76b0cba8da2bdea2b2e902e952f9b
Ping Nie <ping.nie@pku.edu.cn>
2026-09-21 01:14:30 -0400

    [507-P21-B4] Check exact target transport, concrete instance, mutation and axioms

---
+++ research/P21/axioms_b4.lean
@@ -0,0 +1,37 @@
+import NSFormalization.Section4.A04.H1Restart
+import TestSupport.Axioms
+
+#print axioms NSFormalization.Section4.A04.sobolevEnergy_succ_smooth
+run_cmd BlowupDensity.TestSupport.checkAxioms ``NSFormalization.Section4.A04.sobolevEnergy_succ_smooth
+#print axioms NSFormalization.Section4.A04.sobolevEnergy_zero_smooth
+run_cmd BlowupDensity.TestSupport.checkAxioms ``NSFormalization.Section4.A04.sobolevEnergy_zero_smooth
+#print axioms NSFormalization.Section4.A04.sobolevEnergy_one_smooth
+run_cmd BlowupDensity.TestSupport.checkAxioms ``NSFormalization.Section4.A04.sobolevEnergy_one_smooth
+#print axioms NSFormalization.Section4.A04.sobolevEnergy_two_smooth
+run_cmd BlowupDensity.TestSupport.checkAxioms ``NSFormalization.Section4.A04.sobolevEnergy_two_smooth
+#print axioms NSFormalization.Section4.A04.eLpNorm_gradTensor_eq_sqrt
+run_cmd BlowupDensity.TestSupport.checkAxioms ``NSFormalization.Section4.A04.eLpNorm_gradTensor_eq_sqrt
+#print axioms NSFormalization.Section4.A04.enstrophy_differential_on_Icc'
+run_cmd BlowupDensity.TestSupport.checkAxioms ``NSFormalization.Section4.A04.enstrophy_differential_on_Icc'
+#print axioms NSFormalization.Section4.A04.timeShift_force_l2Sq_le
+run_cmd BlowupDensity.TestSupport.checkAxioms ``NSFormalization.Section4.A04.timeShift_force_l2Sq_le
+#print axioms NSFormalization.Section4.A04.classical_manuscriptLocalRegularity
+run_cmd BlowupDensity.TestSupport.checkAxioms ``NSFormalization.Section4.A04.classical_manuscriptLocalRegularity
+#print axioms NSFormalization.Section4.A04.continuousOn_sobolevEnergy
+run_cmd BlowupDensity.TestSupport.checkAxioms ``NSFormalization.Section4.A04.continuousOn_sobolevEnergy
+#print axioms NSFormalization.Section4.A04.differentiableAt_hOneEnergy
+run_cmd BlowupDensity.TestSupport.checkAxioms ``NSFormalization.Section4.A04.differentiableAt_hOneEnergy
+#print axioms NSFormalization.Section4.A04.sobolevENorm_slice_ne_top
+run_cmd BlowupDensity.TestSupport.checkAxioms ``NSFormalization.Section4.A04.sobolevENorm_slice_ne_top
+#print axioms NSFormalization.Section4.A04.hTwo_lintegral_eq
+run_cmd BlowupDensity.TestSupport.checkAxioms ``NSFormalization.Section4.A04.hTwo_lintegral_eq
+#print axioms NSFormalization.Section4.A04.uniform_hTwo_running_bound
+run_cmd BlowupDensity.TestSupport.checkAxioms ``NSFormalization.Section4.A04.uniform_hTwo_running_bound
+#print axioms NSFormalization.Section4.A04.uniform_hTwo_endpoint_bound
+run_cmd BlowupDensity.TestSupport.checkAxioms ``NSFormalization.Section4.A04.uniform_hTwo_endpoint_bound
+#print axioms NSFormalization.Section4.A04.uniform_hOne_lifespan
+run_cmd BlowupDensity.TestSupport.checkAxioms ``NSFormalization.Section4.A04.uniform_hOne_lifespan
+#print axioms NSFormalization.Section4.A04.h1RestartR
+run_cmd BlowupDensity.TestSupport.checkAxioms ``NSFormalization.Section4.A04.h1RestartR
+#print axioms NSFormalization.Section4.A04.h1RestartAt
+run_cmd BlowupDensity.TestSupport.checkAxioms ``NSFormalization.Section4.A04.h1RestartAt
